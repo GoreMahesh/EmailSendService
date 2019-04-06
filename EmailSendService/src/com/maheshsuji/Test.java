9723b95373d3441e67cadbd4994534211ed89abd
Lenovo <barhatesujata13@gmail.com>
2019-04-06 23:48:37 +0530

    after stream api

--- EmailSendService/src/com/maheshsuji/Test.java
+++ EmailSendService/src/com/maheshsuji/Test.java
@@ -9,7 +9,8 @@ public class Test {
 		
 		List<Integer> values=Arrays.asList(12,24,35,46,55);
 
-		values.stream().forEach(System.out::println);//method reference
+		//values.stream().forEach(System.out::println);//method reference
+		System.out.println(	values.stream().filter(i->i%5==0).map(i-> i*2).reduce(0, (c,e) -> c+e));
 	}
 
 }
